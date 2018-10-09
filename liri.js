0541a7f4e5571ce0fbc483008436564c4fe3a7e3
joe <burnett.joe@gmail.com>
2018-10-08 20:12:52 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var keys = require("./keys")
+var spotify = new Spotify(keys.spotify);
+
+// node liri.js concert-this <artist/band name here>
+
+// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
